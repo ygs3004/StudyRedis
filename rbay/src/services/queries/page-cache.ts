4b71a82fe2d162910a5946e9a9c928780fbd40c0
ygs <ygs3004@naver.com>
2025-05-08 02:09:35 +0900

    [Redis] page caching

--- rbay/src/services/queries/page-cache.ts
+++ rbay/src/services/queries/page-cache.ts
@@ -1,3 +1,23 @@
-export const getCachedPage = (route: string) => {};
+import {client} from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = [
+    '/about', '/privacy', '/auth/signin', '/auth/signup'
+];
+
+const pageCacheKeyPrefix = 'pagecache#';
+
+export const getCachedPage = (route: string) => {
+    if (cacheRoutes.includes(route)) {
+        return client.get(pageCacheKeyPrefix + route)
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if (cacheRoutes.includes(route)) {
+        return client.set(pageCacheKeyPrefix + route, page, {
+            EX: 2,
+        });
+    }
+};
