d8739607bf58eefc4f8f01142bcefa21e5ac25a5
ygs <ygs3004@naver.com>
2025-05-19 02:15:45 +0900

    [Redis] pipeline demo

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -1,23 +1,36 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
+import {Result} from "postcss";
 
 const run = async () => {
-    await client.hSet('car', {
+    await client.hSet('car1', {
         color: 'red',
         year: 1950,
-        // engine: { cylinders: 8 },
-        // owner: null || '',
-        // service: undefined || '',
     });
 
-    const car = await client.hGetAll('car');
+    await client.hSet('car2', {
+        color: 'blue',
+        year: 1955,
+    });
 
-    if (Object.keys(car).length === 0) {
-        console.log('Car not found, respond with 404');
-        return;
-    }
+    await client.hSet('car3', {
+        color: 'green',
+        year: 1960,
+    });
 
-    console.log(car);
+
+    // const result = await Promise.all([
+    //     client.hGetAll('car1'),
+    //     client.hGetAll('car2'),
+    //     client.hGetAll('car3')
+    // ]);
+
+    const commnads = [1,2,3].map((id) => {
+        return client.hGetAll(`car${id}`);
+    })
+    const result = await Promise.all(commnads);
+
+    console.log(result);
 };
 
 run();
