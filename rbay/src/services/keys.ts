749ae3cd9b891d68ed1272d09387966cffb09a92
ygs <ygs3004@naver.com>
2025-05-22 02:23:23 +0900

    [Redis] set - 좋아요 표시하기

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -7,3 +7,5 @@ export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 
 export const usernamesUniqueKey = () => `usernames:unique`;
+
+export const userLikesKey = (userId: string) => `users:likes${userId}`;
--- rbay/src/services/queries/likes.ts
+++ rbay/src/services/queries/likes.ts
@@ -1,9 +1,28 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import {client} from "$services/redis";
+import {itemsKey, userLikesKey} from "$services/keys";
+
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+}
+
+
+const itemLikesAttr = 'likes';
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
+    if(inserted){
+        return client.hIncrBy(itemsKey(itemId), itemLikesAttr, 1);
+    }
+    
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+    if (removed) {
+        return client.hIncrBy(itemsKey(itemId), itemLikesAttr, -1);
+    }
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
