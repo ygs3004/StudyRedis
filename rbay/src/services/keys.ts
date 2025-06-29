dfd365e8e61611349777b48d3474605b39b65c85
ygs <ygs3004@naver.com>
2025-06-30 03:00:00 +0900

    [Redis] Sorted Set - 가입유저체크

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -9,3 +9,5 @@ export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => `usernames:unique`;
 
 export const userLikesKey = (userId: string) => `users:likes${userId}`;
+
+export const usernameKey = (userId: string) => `usernames`;
--- rbay/src/services/queries/users.ts
+++ rbay/src/services/queries/users.ts
@@ -1,10 +1,24 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import {client} from "$services/redis";
-import {usernamesUniqueKey, usersKey} from "$services/keys";
+import {usernameKey, usernamesUniqueKey, usersKey} from "$services/keys";
 
 export const getUserByUsername = async (username: string) => {
 
+    // username 을 이용하여 usernames SortedSEet 에서 User Id 체크
+    const decimalId = await client.zScore(usernameKey(), username);
+
+    // 반환값 유무로 회원가입여부 체크
+    if (!decimalId) {
+        throw new Error('존재하지 않는 유저정보입니다.')
+    }
+
+    // 16진수 id 스코어 -> 문자열 id 변환
+    const id = decimalId.toString(16);
+
+    // deserialize hash 반환
+    const user = await client.hGetAll(usersKey(id));
+    return deserialize(id, user);
 };
 
 export const getUserById = async (id: string) => {
@@ -25,6 +39,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
     await client.hSet(usersKey(id), serialize(attrs));
     // 동시섬 문제 없다고 가정
     await client.sAdd(usernamesUniqueKey(), attrs.username);
+    await client.zAdd(usernameKey(), {
+        value: attrs.username,
+        score: parseInt(id, 16) // 16진수 이용 id 문자를 Sorted Set의 스코어로 사용
+    });
 
     return id;
 };
