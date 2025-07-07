d652112221c272a36ceb8966f4f47ee81f5a8e28
ygs <ygs3004@naver.com>
2025-07-08 01:44:35 +0900

    [Redis] relation data - sort & parsing

--- rbay/src/services/queries/items/by-views.ts
+++ rbay/src/services/queries/items/by-views.ts
@@ -1,18 +1,35 @@
 import {client} from "$services/redis";
 import {itemsByViewsKey, itemsKey} from "$services/keys";
+import {deserializeItem} from "$services/queries/items/deserialize";
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-    const results = await client.sort(
+    let results: any = await client.sort(
         itemsByViewsKey(),
         {
             GET: [
-                '#',
-                `${itemsKey('*')}->name`,
+                '#', // id
+                `${itemsKey('*')}->name`, // items#*
                 `${itemsKey('*')}->views`,
+                `${itemsKey('*')}->endingAt`,
+                `${itemsKey('*')}->imageUrl`,
+                `${itemsKey('*')}->price`,
             ],
-            BY: 'score',
+            BY: 'nosort', // nosort => 기본 순서 사용, Sorted Set
+            DIRECTION: order, // 정렬방향
+            LIMIT:{  // 데이터 수
+                offset,
+                count,
+            }
         }
     );
 
-    console.log(results);
+    const items = [];
+    while (results.length) {
+        const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+        const item = deserializeItem(id, {id, name, views, endingAt, imageUrl, price});
+        items.push(item);
+        results = rest;
+    }
+
+    return items;
 };
