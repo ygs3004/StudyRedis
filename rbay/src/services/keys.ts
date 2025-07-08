246f931ea3ad121d1307aea0f62053bb7d66f855
ygs <ygs3004@naver.com>
2025-07-09 02:15:31 +0900

    HyperLogsLogs - views count

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -14,3 +14,4 @@ export const usernameKey = () => `usernames`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const itemsByViewsKey = () => `items:views`;
 export const itemsByEndingAtKey = () => `items:endingAt`;
+export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`
--- rbay/src/services/queries/views.ts
+++ rbay/src/services/queries/views.ts
@@ -1,12 +1,14 @@
-import {itemsByViewsKey, itemsKey} from "$services/keys";
+import {itemsByViewsKey, itemsKey, itemsViewsKey} from "$services/keys";
 import {client} from "$services/redis";
 
 export const incrementView = async (itemId: string, userId: string) => {
-    const beforeScore = await client.zScore(itemsByViewsKey(), itemId);
-    console.log(beforeScore);
+    // HyperLogsLogs, 오차율이 꽤나 존재하지만 공간적인 효율이 높아 조회수 같은 정확성이 비교적 덜 중요한 경우 사용
+    const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
 
-    return Promise.all([
-        client.hIncrBy((itemsKey(itemId)), 'views', 1),
-        client.zIncrBy(itemsByViewsKey(), 1, itemId)
-    ]);
+    if(inserted) {
+        return Promise.all([
+            client.hIncrBy((itemsKey(itemId)), 'views', 1),
+            client.zIncrBy(itemsByViewsKey(), 1, itemId)
+        ]);
+    }
 };
