32ae9099892f829e053a5ec0a83264f64e92dedd
ygs <ygs3004@naver.com>
2025-08-08 02:08:23 +0900

    redis - withLock Function

--- rbay/src/services/redis/lock.ts
+++ rbay/src/services/redis/lock.ts
@@ -1,4 +1,28 @@
-export const withLock = async () => {};
+import {randomBytes} from "crypto";
+import {client} from "$services/redis/client";
+
+export const withLock = async (key: string, callback: () => any) => {
+    const retryDelayMs = 100;
+    let retries = 20;
+
+    const token = randomBytes(6).toString('hex');
+    const lockKey = `lock:${key}`;
+
+    while(retries-- >= 0) {
+        const acquired = await client.set(lockKey, token, {
+            NX:true,
+        });
+
+        if (!acquired) {
+            await pause(retryDelayMs);
+            continue;
+        }
+
+        const result = await callback();
+        await client.del(lockKey);
+        return result;
+    }
+};
 
 const buildClientProxy = () => {};
 
