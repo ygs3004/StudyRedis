6a44c816e0891d3e61da374a3c6cba9b9a7a8d22
ygs <ygs3004@naver.com>
2025-08-20 02:02:23 +0900

    redis - RedisSearch fuzzy search

--- rbay/src/services/queries/items/search.ts
+++ rbay/src/services/queries/items/search.ts
@@ -1,1 +1,31 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import {itemsIndexKey} from "$services/keys";
+import {client} from "$services/redis";
+import {deserializeItem} from "$services/queries/items/deserialize";
+
+export const searchItems = async (term: string, size: number = 5) => {
+    
+    //fuzzy search example, RedisSearch 의 fuzzy search 는 한글은 안됨ㅠㅠ... Elasticsearch 검색 시스템 필요
+    const cleaned = term
+        .replaceAll(/[^ㄱ-ㅎ가-힣a-zA-Z0-9 ]/g, "")
+        .trim()
+        .split(" ")
+        .map(word => word ? `%${word}%` : word)
+        .join (' ');
+
+    if(cleaned === ''){
+        return [];
+    }
+
+    const results = await client.ft.search(
+        itemsIndexKey(),
+        cleaned,
+        {
+            LIMIT: {
+                from: 10,
+                size
+            }
+        }
+    )
+
+    return results.documents.map(({id, value}) => deserializeItem(id, value as any))
+};
